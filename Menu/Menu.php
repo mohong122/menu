f5c63be2662b3e36991a1e4c9773678ea4172e58
崔彦鹏 <mohong122@qq.com>
2017-03-29 11:19:20 +0800

    初始化项目

--- Menu/Menu.php
+++ Menu/Menu.php
@@ -71,20 +71,3 @@ class Menu
 
 
 
-class TableAction
-{
-    public $title;
-    public $key;
-    public $icon = "";
-    public $type = "";  //可选参数modal,confirm
-    public $modal;
-    public $confirm;
-    public $condition;  //条件 例如 %s > 1 @status
-
-    function __construct($key, $title, $type = "modal")
-    {
-        $this->key = $key;
-        $this->title = $title;
-        $this->type = $type;
-    }
-}
---
+++ Menu/TableAction.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohong
+ * Date: 2017/3/29
+ * Time: 11:18
+ */
+
+namespace Menu;
+
+
+class TableAction
+{
+    public $title;
+    public $key;
+    public $icon = "";
+    public $type = "";  //可选参数modal,confirm
+    public $modal;
+    public $confirm;
+    public $condition;  //条件 例如 %s > 1 @status
+
+    function __construct($key, $title, $type = "modal")
+    {
+        $this->key = $key;
+        $this->title = $title;
+        $this->type = $type;
+    }
+}
